612719c7bd12015a3a41af7895c29b201d979a39
amandue10 <50896687+amandue10@users.noreply.github.com>
2021-08-01 13:38:32 -0600

    Working Userstory 1 and 2

--- src/com/skilldistillery/foodtruck/app/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruck.java
@@ -1,26 +1,123 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
 public class FoodTruck {
+	private String foodTruckName;
+	private String foodType;
+	private int truckRating;
+	private int uniqueId = 0;
+	private static int nextId = 1;
+	private int counter;
 
-	int uniqueID;
-	String truckName;
-	String foodType;
-	int rating;
+	private FoodTruck foodTruckArr[] = new FoodTruck[5];
 
-	// food truck constructor
-	public FoodTruck(String foodTruck, int uniqueId2, String typeFood, int stars) {
-		// its field is set to the users input
-		truckName = foodTruck;
-		foodType = typeFood;
-		rating = stars;
-		uniqueID = uniqueId2;
+	public void truckInput(Scanner input) {
+		String truckName, foodType;
+		int truckRating, i;
+
+		for (i = 0; i < foodTruckArr.length; i++) {
+			System.out.println("Enter name of food truck you would like to rate:");
+			truckName = input.next();
+			if (truckName.equalsIgnoreCase("quit")) {
+				break;
+			}
+			foodTruckArr[i] = new FoodTruck();
+			foodTruckArr[i].setTruckName(foodTruckName);
+
+			System.out.println("Enter type of food:");
+			foodType = input.next();
+			foodTruckArr[i].setFoodType(foodType);
+
+			System.out.println("Enter Truck Star Rating, 1 through 5 ");
+			truckRating = input.nextInt();
+			foodTruckArr[i].setTruckRating(truckRating);
+			if (truckRating > 5) {
+				System.out.println("Enter a whole number between 1 and 5");
+				truckRating = input.nextInt();
+			}
+			// set id for each array
+			foodTruckArr[i].setId(i);
+			counter++;
+		}
+	}
+
+	// constructor
+	public FoodTruck(String truckName, String foodType, int truckRating, FoodTruck[] foodTruckArr) {
+		this.foodTruckName = truckName;
+		this.foodType = foodType;
+		this.truckRating = truckRating;
+		this.uniqueId = nextId;
+	}
+
+	public FoodTruck() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setTruckName(String foodTruckName2) {
 
 	}
 
-	@Override
-	public String toString() {
-		return "FoodTruck [uniqueID=" + uniqueID + ", truckName=" + truckName + "," + " foodType=" + foodType
-				+ ", rating=" + rating + "]";
+//blueprint for ID Number generator
+	public void setId(int i) {
+		this.uniqueId = nextId;
+		nextId++;
+	}
+
+	public String getFoodTruckName() {
+		return foodTruckName;
+	}
+
+	public void setFoodTruckName(String foodTruckName) {
+		this.foodTruckName = foodTruckName;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public int getTruckRating() {
+		return truckRating;
+	}
+
+	public void setTruckRating(int truckRating) {
+		this.truckRating = truckRating;
+	}
+
+	public int getUniqueId() {
+		return uniqueId;
+	}
+
+	public void setUniqueId(int uniqueId) {
+		uniqueId = uniqueId;
+	}
+
+	public static int getNextId() {
+		return nextId;
+	}
+
+	public static void setNextId(int nextId) {
+		FoodTruck.nextId = nextId;
+	}
+
+	public FoodTruck[] getFoodTruckArr() {
+		return foodTruckArr;
+	}
+
+	public void setFoodTruckArr(FoodTruck[] foodTruckArr) {
+		this.foodTruckArr = foodTruckArr;
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public void setCounter(int counter) {
+		this.counter = counter;
 	}
 
 }
--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,34 +1,14 @@
 package com.skilldistillery.foodtruck.app;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-		String truckName;
-		int uniqueId;
-		int rating;
-		String foodType;
-		final int MAX = 99;
-
-		Scanner keyboard = new Scanner(System.in);
-		Random rand = new Random();
-
-		uniqueId = rand.nextInt(MAX);
-
-		System.out.println("Enter the name of the food truck");
-		truckName = keyboard.nextLine();
-
-		System.out.println("Enter food type");
-		foodType = keyboard.nextLine();
-
-		System.out.println("Enter your rating using numbers 1 through 5");
-		rating = keyboard.nextInt();
-
-		FoodTruck foodTruck1 = new FoodTruck(truckName, uniqueId, foodType, rating);
-		System.out.println(foodTruck1.toString());
+		FoodTruck foodTruck = new FoodTruck();
+		foodTruck.truckInput(input);
 
 	}
 
